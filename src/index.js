a8d9b255f94f93e4094f5032dfc0ea9f2b658105
Laura Castañeda <laura.castaeda95@gmail.com>
2021-12-01 18:59:42 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ const app = express();
 const morgan = require ('morgan');
 const cors = require ('cors');
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 require('./database')
 
 app.use(morgan('dev'));
