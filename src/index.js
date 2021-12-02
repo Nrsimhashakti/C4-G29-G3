e854143eabc891c10c8cdf0333b087ab1cbd0124
Laura Castañeda <laura.castaeda95@gmail.com>
2021-12-01 19:23:52 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ require('./database')
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors({ origin:'http://localhost:3000/'}));
+app.use(cors({ origin:'*'}));
 
 
 //RUTAS
