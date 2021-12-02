78c7385a02861a6224af22edfe9aec5c55248c5b
Laura Castañeda <laura.castaeda95@gmail.com>
2021-12-02 11:11:20 -0500

    make it better

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -24,7 +24,7 @@ export const Inicio = () => {
     return(
         <>
          
-         <div className="container_12">
+         <div className="">
             {/* CARRUSEL */}
             
             <Carousel>
