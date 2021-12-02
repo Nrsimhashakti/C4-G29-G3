b44760dbe2eaa9802a075622471e4fcb9073e52f
Laura Castañeda <laura.castaeda95@gmail.com>
2021-12-01 19:16:31 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ const app = express();
 const morgan = require ('morgan');
 const cors = require ('cors');
 
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:4000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
-
 require('./database')
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors({ origin:'*'}));
+app.use(cors({ origin:'http://localhost:3000/'}));
 
 
 //RUTAS
